98d56f88f735a0026f1eaef6db66a909ed1240db
Sean Bass <seandbass@gmail.com>
2019-11-07 12:27:47 -0800

    Update nnFoo.py

--- nnFoo.py
+++ nnFoo.py
@@ -6,3 +6,14 @@ import matplotlib.pyplot as plt
 data = keras.datasets.fashion_mnist
 
 (train_images, train_labels), (test_images, test_labels) = data.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+#normalize data (so data in array is # value between 0 and 1 instead of 0 and 255)
+train_images = train_images/255.0
+test_images = test_images/255.0
+
+#example to show image in dataset
+plt.imshow(train_images[0], cmap=plt.cm.binary)
+plt.show()
