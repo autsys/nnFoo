63f506b921930fc9f66bf0633a2a441d7a7a8702
Sean Bass <seandbass@gmail.com>
2019-11-08 14:27:40 -0800

    Update nnFoo.py

--- nnFoo.py
+++ nnFoo.py
@@ -17,3 +17,22 @@ test_images = test_images/255.0
 #example to show image in dataset
 plt.imshow(train_images[0], cmap=plt.cm.binary)
 plt.show()
+
+#hidden layers should be roughly 15-20% amount of imput neurons
+#for example in this we have 784 input neurons (28x28) so we
+#can add 128 hidden layers
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)), #flatten (compresses the 2d array)
+    keras.layers.Dense(128, activation='relu'), #hidden layer - rectified linear activaton
+    keras.layers.Dense(10, activation='softmax') #output layer - softmax 0-1 
+])
+
+#look these up (adam, sparse, the metrics etc..)
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=["accuracy"])
+
+#epoch is the amount of times the entire data set is ran through (how many times each image is seen)
+model.fit(train_images, train_labels, epochs=5)
+
+
+
